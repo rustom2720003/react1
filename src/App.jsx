8587d6baf315adb97259d1a195f68781436087d8
Rusi-27 <rustom2720002@gmail.com>
2026-04-18 13:26:03 +0530

    adding components folder

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className='card1'>
+        <h1>Rustom Singh Yadav</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum expedita delectus impedit laborum minus enim incidunt fugiat. Doloremque maiores laborum perferendis officiis consequuntur blanditiis ab dicta nisi, eligendi, iure earum.</p>
+
+      </div>
+      <div className='card2'>
+        <h1>Prachi Rajput</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum expedita delectus impedit laborum minus enim incidunt fugiat. Doloremque maiores laborum perferendis officiis consequuntur blanditiis ab dicta nisi, eligendi, iure earum.</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
